98bc4e3648655780f726fa7bc0ca5e0ad681ca2e
99Atharva <atharvatilvankar@gmai.com>
2021-04-10 08:40:23 +0530

    l

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -3,7 +3,7 @@ import './Search.css';
 
 const Search = ( {handleInput , search})=> {
   return (
-      <div className='pa5 '>
+      <div className='pa5'>
                       
             <input className='box pa3 tc pr4 pl4 ba b--silver bw2 br3' type='text'
              placeholder='Tell me your fav Series'
