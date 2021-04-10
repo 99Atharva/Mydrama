8cad066fc264706a073d10731895bc89434a3ee9
99Atharva <atharvatilvankar@gmai.com>
2021-04-10 08:03:59 +0530

    Updates

---
+++ precache-manifest.803e222d21e4f02ec4012cf4f678ac9f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b66d764133303a7502f92669dc548af2",
+    "url": "/Mydrama/index.html"
+  },
+  {
+    "revision": "641cc9eecb784f70d5cb",
+    "url": "/Mydrama/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0bc3d27f154a1a08de56",
+    "url": "/Mydrama/static/css/main.1d1c7f7f.chunk.css"
+  },
+  {
+    "revision": "641cc9eecb784f70d5cb",
+    "url": "/Mydrama/static/js/2.169b6566.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Mydrama/static/js/2.169b6566.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bc3d27f154a1a08de56",
+    "url": "/Mydrama/static/js/main.ef1488d8.chunk.js"
+  },
+  {
+    "revision": "6f9d37a18df9d1ffde99",
+    "url": "/Mydrama/static/js/runtime-main.b45a5c99.js"
+  },
+  {
+    "revision": "f86fa3ca08084b33e7d6a264165b6cf9",
+    "url": "/Mydrama/static/media/Web.f86fa3ca.jpg"
+  }
+]);
